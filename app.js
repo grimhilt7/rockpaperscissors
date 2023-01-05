c82610636701836d96dddb4f0ebb6a9bb6c39f35
grimhilt7 <116620023+grimhilt7@users.noreply.github.com>
2023-01-06 07:46:31 +0900

    Complete the project

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const selects = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+const playerScore = document.querySelector('#score-player');
+const computerScore = document.querySelector('#score-computer');
+
 // a factory function to create a player
 function createPlayers(name) {
   const id = name;
@@ -13,6 +18,8 @@ function createPlayers(name) {
 const player = createPlayers("player");
 const computer = createPlayers("computer");
 
+
+
 // randomly returns either r, p, or s (rock, paper, and scissor accordingly).
 function getComputerChoice() {
   const num = Math.random();
@@ -24,56 +31,57 @@ function getComputerChoice() {
   }
   if (num > 0.66 && num <= 1) {
     return "s"; }
-}
-
-function algorithm(choiceOne, choiceTwo) {
-  const str = [choiceOne, choiceTwo].sort().join('')
-  switch (str) {
-    case "rs": return "r";
-    case "ps": return "s";
-    case "pr": return "p";
-    default: return null;
   }
-};
-
-// plays a single round game, and returns a string that determines the winner
-function play(playerOne, playerTwo) {
-  const result = algorithm(playerOne.choice, playerTwo.choice);
-  if (playerOne.choice === result) {
-    playerOne.win();
-    console.log(`${playerOne.id} won!`);
+  
+  function algorithm(choiceOne, choiceTwo) {
+    const str = [choiceOne, choiceTwo].sort().join('')
+    switch (str) {
+      case "rs": return "r";
+      case "ps": return "s";
+      case "pr": return "p";
+      default: return null;
+    }
+  };
+  
+  // plays a single round game, and returns a string that determines the winner
+  function play(playerOne, playerTwo) {
+    const outcome = algorithm(playerOne.choice, playerTwo.choice);
+    if (playerOne.choice === outcome) {
+      playerOne.win();
+      return`${playerOne.id.charAt(0).toUpperCase() + playerOne.id.slice(1)} won!`;
+    }
+    if (playerTwo.choice === outcome) {
+      playerTwo.win();
+      return`${playerTwo.id.charAt(0).toUpperCase() + playerTwo.id.slice(1)} won!`;
+    }
+    if (!outcome) {
+      return "Draw!"
+    }
   }
-  if (playerTwo.choice === result) {
-    playerTwo.win();
-    console.log(`${playerTwo.id} won!`);
-  }
-}
 
-// plays five games in the condition of three wins first to win, and returns a string that determines the final winner
-function game() {
-  [player.winCount, computer.winCount] = [0, 0];
-
-  for (let i=0; i <= 4; i++) {
-    player.choice = prompt("...");
+  function game() {
+    const temp = this.dataset.id.charAt(0);
+    player.choice = temp;
     computer.choice = getComputerChoice();
+    result.textContent = play(player, computer);
+  
+    playerScore.textContent = player.winCount;
+    computerScore.textContent = computer.winCount;
 
-    play(player, computer)
-
-    if (player.winCount >= 3) {
-      return player;
+    if (player.winCount >= 5) {
+      const winner = player.id.charAt(0).toUpperCase() + player.id.slice(1);
+      player.winCount = 0;
+      computer.winCount = 0;
+      alert(`${winner} is the final winner!`)
     }
-    if (computer.winCount >= 3) {
-      return computer;
+    if (computer.winCount >= 5) {
+      const winner = computer.id.charAt(0).toUpperCase() + computer.id.slice(1);
+      player.winCount = 0;
+      computer.winCount = 0;
+      alert(`${winner} is the final winner!`)
     }
   }
-
-  if (player.winCount > computer.winCount) {
-    return player
-  }
-  if (player.winCount < computer.winCount) {
-    return computer
-  } 
-  if (player.winCount === computer.winCount) {
-    return "DRAW"
-  }
-}
+  
+  selects.forEach(select => {
+    select.addEventListener('click', game)
+  })
